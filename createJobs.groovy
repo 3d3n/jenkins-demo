44a892f3ac521d95083297ca6c0699ba49f9880d
Ajibade Eden <teejay.2005@yahoo.com>
2021-12-29 00:23:40 -0600

    theme-park-job project pipeline

--- createJobs.groovy
+++ createJobs.groovy
@@ -6,3 +6,17 @@ pipelineJob('pipelineJob') {
         }
     }
 }
+pipelineJob('theme-park-job') {
+    definition {
+        cpsScm {
+            scm {
+                git {
+                    remote {
+                        url 'https://github.com/3d3n/spring-boot-api-example.git'
+                    }
+                    branch 'main'
+                }
+            }
+        }
+    }
+}
